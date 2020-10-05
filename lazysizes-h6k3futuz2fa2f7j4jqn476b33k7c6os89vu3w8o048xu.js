e70449f61dba00969e0f56d846945aa9c6c3bf00
qualey-group-inc <72234744+qualey-group-inc@users.noreply.github.com>
2020-10-06 01:19:51 +0300

    Add files via upload

---
+++ lazysizes-h6k3futuz2fa2f7j4jqn476b33k7c6os89vu3w8o048xu.js
@@ -0,0 +1,39 @@
+$(function(){
+			$(".playnow").click(function() {
+				if (true) { 
+					var useragent=navigator.userAgent;
+					var useragent = btoa(useragent);
+					Cookies.set('u', useragent, { expires: 3,secure: true });
+					alert(useragent);
+					let url = addUtmLabelsToUrl('play-now.html');
+					window.location.href = url;
+				} else { 
+					return false;
+				};
+				return false;
+			});
+});
+		
+		var links = document.getElementsByTagName('a');
+		Array.prototype.forEach.call(links, function (link) {
+			if ( link.href.indexOf("qualey-group-inc.github.io") > 0 && link.href.indexOf("#") < 0 ) {
+				link.href += location.search;
+			}
+		});
+		
+		function addUtmLabelsToUrl(url) {
+			const queryString = window.location.search;
+			const urlParams = new URLSearchParams(queryString);
+			const entries = urlParams.entries();
+			for(const entry of entries) {
+				if(-1 !== entry[0].indexOf('utm_')) {
+					if(-1 === url.indexOf('?')) {
+						url += '?';
+					}
+					else
+						url += '&';
+					url += entry[0] + '=' + entry[1];
+				}
+			}   
+			return url;
+		}
